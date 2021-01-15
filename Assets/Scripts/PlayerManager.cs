2e31f3cdf9d46676868bd4b39bb10c17712f2202
as <>
2021-01-15 22:01:16 +0900

    プレイヤーを左右に動かす

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public enum DIRECTION_TYPE
+    {
+        STOP,
+        RIGHT,
+        LEFT,
+    }
+
+    DIRECTION_TYPE direction = DIRECTION_TYPE.STOP;
+
+    Rigidbody2D rigidbody2D;
+
+    float speed;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        if (x == 0)
+        {
+            //止まっている
+            direction = DIRECTION_TYPE.STOP;
+        }
+        else if (x > 0)
+        {
+            //右へ
+            direction = DIRECTION_TYPE.RIGHT;
+        }
+        else if (x < 0)
+        {
+            //左へ
+            direction = DIRECTION_TYPE.LEFT;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        switch (direction)
+        {
+            case DIRECTION_TYPE.STOP:
+                speed = 0;
+                break;
+            case DIRECTION_TYPE.RIGHT:
+                speed = 3;
+                transform.localScale = new Vector3(1, 1, 1);
+                break;
+            case DIRECTION_TYPE.LEFT:
+                speed = -3;
+                transform.localScale = new Vector3(-1, 1, 1);
+                break;
+        }
+        rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+    }
+}
