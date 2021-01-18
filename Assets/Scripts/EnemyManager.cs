698bf01ae657728e84526fa5c510f56b2cd83f54
as <>
2021-01-18 15:29:02 +0900

    敵を右に移動させる

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public enum DIRECTION_TYPE
+    {
+        STOP,
+        RIGHT,
+        LEFT,
+    }
+
+    DIRECTION_TYPE direction = DIRECTION_TYPE.STOP;
+
+    Rigidbody2D rigidbody2D;
+    float speed;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        //右へ
+        direction = DIRECTION_TYPE.RIGHT;
+    }
+
+    private void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        switch (direction)
+        {
+            case DIRECTION_TYPE.STOP:
+                speed = 0;
+                break;
+            case DIRECTION_TYPE.RIGHT:
+                speed = 3;
+                transform.localScale = new Vector3(1, 1, 1);
+                break;
+            case DIRECTION_TYPE.LEFT:
+                speed = -3;
+                transform.localScale = new Vector3(-1, 1, 1);
+                break;
+        }
+        rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+    }
+}
